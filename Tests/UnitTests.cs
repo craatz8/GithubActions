3dcd0e06fe50f6e2d1d367ba084c239fa973cf9c
Connor <craatz8@gmail.com>
2024-11-08 02:02:01 -0600

    Fixed Function Names

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace GithubActionsLab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_ValidRaatz()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_InvalidRaatz()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_NullRaatz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -32,7 +32,7 @@ namespace GithubActionsLab
 
         // Implement 3 tests per operation, following a similar pattern as above
         [Test]
-        public void Subtract_Valid()
+        public void Subtract_ValidRaatz()
         {
             Assert.AreEqual(2, Program.Subtract("5", "3"));
             Assert.AreEqual(0, Program.Subtract("2", "2"));
@@ -40,7 +40,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Subtract_Invalid()
+        public void Subtract_InvalidRaatz()
         {
             Assert.Throws<FormatException>(() => Program.Subtract("5", "a"));
             Assert.Throws<FormatException>(() => Program.Subtract("a", "3"));
@@ -48,7 +48,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Subtract_Null()
+        public void Subtract_NullRaatz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Subtract("5", null));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "3"));
@@ -57,7 +57,7 @@ namespace GithubActionsLab
 
         // Tests for Multiply
         [Test]
-        public void Multiply_Valid()
+        public void Multiply_ValidRaatz()
         {
             Assert.AreEqual(12, Program.Multiply("5", "3"));
             Assert.AreEqual(0, Program.Multiply("0", "3"));
@@ -65,7 +65,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Multiply_Invalid()
+        public void Multiply_InvalidRaatz()
         {
             Assert.Throws<FormatException>(() => Program.Multiply("5", "a"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "3"));
@@ -73,7 +73,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Multiply_Null()
+        public void Multiply_NullRaatz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("5", null));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "3"));
@@ -82,7 +82,7 @@ namespace GithubActionsLab
 
         // Tests for Divide
         [Test]
-        public void Divide_Valid()
+        public void Divide_ValidRaatz()
         {
             Assert.AreEqual(2, Program.Divide("6", "3"));
             Assert.AreEqual(5, Program.Divide("10", "2"));
@@ -90,7 +90,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Divide_Invalid()
+        public void Divide_InvalidRaatz()
         {
             Assert.Throws<FormatException>(() => Program.Divide("6", "a"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "3"));
@@ -98,7 +98,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Divide_Null()
+        public void Divide_NullRaatz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("6", null));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "3"));
@@ -106,14 +106,14 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Divide_ByZero()
+        public void Divide_ByZeroRaatz()
         {
             Assert.Throws<DivideByZeroException>(() => Program.Divide("6", "0"));
         }
 
         // Tests for Power
         [Test]
-        public void Power_Valid()
+        public void Power_ValidRaatz()
         {
             Assert.AreEqual(8, Program.Power("2", "3"));
             Assert.AreEqual(1, Program.Power("5", "0"));
@@ -121,7 +121,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Power_Invalid()
+        public void Power_InvalidRaatz()
         {
             Assert.Throws<FormatException>(() => Program.Power("2", "a"));
             Assert.Throws<FormatException>(() => Program.Power("a", "3"));
@@ -129,7 +129,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Power_Null()
+        public void Power_NullRaatz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Power("2", null));
             Assert.Throws<ArgumentNullException>(() => Program.Power(null, "3"));
